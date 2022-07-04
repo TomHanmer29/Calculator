335de788b14c544a0b028339e3e5de5cb0b67ee7
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-04 17:22:48 +0100

    Made it so you can operate on any number of numbers and stored them in an array for future reference

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -13,30 +13,40 @@ namespace Calculator
             int operatorIndex = Array.IndexOf(validInputs,operatorInput);
             if(operatorIndex>=0)
             {
-                Console.WriteLine("Enter first number:");
-                string numOneInput = Console.ReadLine();
-                Console.WriteLine("Enter second number:");
-                string numTwoInput = Console.ReadLine();
-                double numOne = double.Parse(numOneInput);
-                double numTwo = double.Parse(numTwoInput);
-                double result = 0;
-                switch (operatorIndex)
+                Console.WriteLine("How many numbers would you like to "+validInputs[operatorIndex]+" ?");
+                string numOfNumsInput = Console.ReadLine();
+                int numOfNums = int.Parse(numOfNumsInput);
+                double[] nums = new double[numOfNums];
+                Console.WriteLine("Enter number 1:");
+                string firstNum = Console.ReadLine();
+                double result = double.Parse(firstNum);
+                nums[0] = result;
+                for (int currentNum = 1; currentNum < numOfNums; currentNum++)
                 {
-                    case 0:
-                        result = numOne + numTwo;
-                        break;
-                    case 1:
-                        result = numOne * numTwo;
-                        break;
-                    case 2:
-                        result = numOne / numTwo;
-                        break;
-                    case 3:
-                        result = numOne - numTwo;
-                        break;
+                    Console.WriteLine("Enter number "+(currentNum+1)+":");
+                    string numInput = Console.ReadLine();
+                    nums[currentNum] = double.Parse(numInput);
+                    
+                    switch (operatorIndex)
+                    {
+                        case 0:
+                            result += double.Parse(numInput);
+                            break;
+                        case 1:
+                            result *= double.Parse(numInput);
+                            break;
+                        case 2:
+                            result /= double.Parse(numInput);
+                            break;
+                        case 3:
+                            result -= double.Parse(numInput);
+                            break;
+                    }
+
                 }
+                
                 string strResult = result.ToString();
-                Console.WriteLine(numOneInput+" "+validInputs[operatorIndex]+" "+numTwoInput+" = "+strResult);
+                Console.WriteLine("The answer is: "+strResult);
             }
             else
             {
