8936d92619b0f0e834f10c70d53b3b13c0cbc79d
Tom Hanmer <Tom.Hanmer@softwire.com>
2022-07-04 15:28:50 +0100

    Add a simple multiplier

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
+            
+            Console.WriteLine("Enter first number:");
+            string numOneInput = Console.ReadLine();
+            Console.WriteLine("Enter second number:");
+            string numTwoInput = Console.ReadLine();
+            double numOne = double.Parse(numOneInput);
+            double numTwo = double.Parse(numTwoInput);
+            double product = numOne * numTwo;
+            Console.WriteLine("The product of the two numbers is "+product);
         }
     }
 }
